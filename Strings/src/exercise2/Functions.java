9829da94399f9c5ef61a5b244892e559960f00ea
aleleotta <alessandro.leotta@iesnervion.es>
2023-02-20 10:59:03 +0100

    .

--- Strings/src/exercise2/Functions.java
+++ Strings/src/exercise2/Functions.java
@@ -3,7 +3,15 @@ import java.util.*;
 
 public class Functions {
 	
-	public static void d() {
+	public static void PasswordGuessingGame() {
+		String password;
+		String guess;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Player 1:\nIntroduce a password: ");
+		password = sc.next();
+		System.out.print("Player 2:\nIntroduce a guess: ");
+		guess = sc.next();
+		sc.close();
 	}
 }
 
@@ -11,4 +19,7 @@ public class Functions {
  *La mecánica del juego es la siguiente: el primer jugador introduce la contraseña;
  *a continuación, el segundo jugador debe teclear palabras hasta que la acierte.
  *El juego debe indicar si la palabra introducida es mayor o menor alfabéticamente que la contraseña.
+ *
+ *VERSIÓN AVANZADA: El juego, en vez de indicar si la palabra es mayor o menor alfabéticamente que la contraseña,
+ *debe mostrar una cadena con los caracteres acertados en sus lugares respectivos y asteriscos en los no acertados.
  */
--- Strings/src/exercise2/Main.java
+++ Strings/src/exercise2/Main.java
@@ -12,4 +12,7 @@ public class Main {
  *La mecánica del juego es la siguiente: el primer jugador introduce la contraseña;
  *a continuación, el segundo jugador debe teclear palabras hasta que la acierte.
  *El juego debe indicar si la palabra introducida es mayor o menor alfabéticamente que la contraseña.
+ *
+ *VERSIÓN AVANZADA: El juego, en vez de indicar si la palabra es mayor o menor alfabéticamente que la contraseña,
+ *debe mostrar una cadena con los caracteres acertados en sus lugares respectivos y asteriscos en los no acertados.
  */
