bdb70a8f981aac85f9c699bf71768ef82397e949
aleleotta <alessandro.leotta@iesnervion.es>
2023-02-20 09:45:26 +0100

    .

--- Strings/src/exercise1/Functions.java
+++ Strings/src/exercise1/Functions.java
@@ -2,4 +2,19 @@ package exercise1;
 import java.util.*;
 
 public class Functions {
+	
+	public static void shorterString() {
+		Scanner sc = new Scanner(System.in);
+		String firstString;
+		String secondString;
+		System.out.print("Please introduce the first random sentence: ");
+		firstString = sc.nextLine();
+		System.out.print("Please introduce the second random sentence: ");
+		secondString = sc.nextLine();
+		if(firstString.length() < secondString.length()) {
+			System.out.println("\nThe first sentence is shorter than the second sentence.");
+		} else {
+			System.out.println("\nThe second sentence is shorter than the first sentence.");
+		}
+	}
 }
