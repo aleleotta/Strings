cdccf5accc3a4aa82b65952205874c11f6617408
aleleotta <alessandro.leotta@iesnervion.es>
2023-02-22 13:37:11 +0100

    .

--- Strings/src/exercise2/Functions.java
+++ Strings/src/exercise2/Functions.java
@@ -3,14 +3,23 @@ import java.util.*;
 
 public class Functions {
 	
-	public static void PasswordGuessingGame() {
+	public static void passwordGuessingGame() {
 		String password;
 		String guess;
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Player 1:\nIntroduce a password: ");
 		password = sc.next();
-		System.out.print("Player 2:\nIntroduce a guess: ");
-		guess = sc.next();
+		do {
+			System.out.print("Player 2:\nIntroduce a guess: ");
+			guess = sc.next();
+			if(password.compareTo(guess) < 0) {
+				System.out.println("String resulted with a negative value.");
+			} else if(password.compareTo(guess) > 0) {
+				System.out.println("String resulted with a positive value.");
+			}
+		}
+		while(!password.equals(guess));
+		System.out.println("Congratulations! You guessed Player 1's password!");
 		sc.close();
 	}
 }
