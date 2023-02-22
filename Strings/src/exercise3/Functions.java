7ef8149c4f446d105fb840ed87bbdab44133267c
aleleotta <alessandro.leotta@iesnervion.es>
2023-02-22 14:21:48 +0100

    .

--- Strings/src/exercise3/Functions.java
+++ Strings/src/exercise3/Functions.java
@@ -2,6 +2,22 @@ package exercise3;
 import java.util.*;
 
 public class Functions {
+	
+	public static int checkBlanks(){
+		String sentence;
+		int blanks = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please introduce a sentence: ");
+		sentence = sc.nextLine();
+		for(int index = 0; index < sentence.length(); index++) {
+			char character = sentence.charAt(index);
+			if(character == ' ') {
+				blanks++;
+			}
+		}
+		sc.close();
+		return blanks;
+	}
 }
 
 /*Diseña una aplicación que pida al usuario que introduzca una frase por teclado
--- Strings/src/exercise9/Functions.java
+++ Strings/src/exercise9/Functions.java
@@ -3,3 +3,15 @@ import java.util.*;
 
 public class Functions {
 }
+
+/*Disponemos de la siguiente relación de letras:
+(Mirar enunciado para ver un mejor esquema.)
+conjunto 1: e i k m p q r s t u v
+conjunto 2: p v i u m t e r k q s
+
+mediante la cual es posible codificar un texto, convirtiendo cada letra del conjunto 1, en su correspondiente del conjunto 2. El resto de las letras no se modifican. Los conjuntos se utilizan tanto para codificar mayúsculas como minúsculas, mostrando siempre la codificación en minúsculas.
+Un ejemplo: la palabra «PaquiTo» se codifica como «matqvko».
+Se pide realizar un programa codificador, en el que se implemente la función:
+char codifica(char conjunto1[],char conjunto2[], char c)
+que devuelve el carácter c codificado según los conjuntos 1 y 2 que se le pasan.
+*/
