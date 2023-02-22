87dad80b6bed2fa7d9514427fb5db48e1a3449a2
aleleotta <alessandro.leotta@iesnervion.es>
2023-02-22 14:39:29 +0100

    .

--- Strings/src/exercise4/Functions.java
+++ Strings/src/exercise4/Functions.java
@@ -2,6 +2,15 @@ package exercise4;
 import java.util.*;
 
 public class Functions {
+	
+	public static void invertString() {
+		String input;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please introduce a sentence: ");
+		input = sc.nextLine();
+		for(int index = 0; index < input.length(); index++) {}
+		sc.close();
+	}
 }
 
 /*Diseñar una función a la que se le pase una cadena de caracteres y la devuelva invertida.
