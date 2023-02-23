3c903f315332852b1216045e34426a6fec301a22
aleleotta <alessandro.leotta@iesnervion.es>
2023-02-23 14:39:09 +0100

    .

--- Strings/src/exercise5/Functions.java
+++ Strings/src/exercise5/Functions.java
@@ -2,6 +2,18 @@ package exercise5;
 import java.util.*;
 
 public class Functions {
+	
+	public static void concatenate() {
+		String string1 = "";
+		String string2 = "";
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduce the first substring: ");
+		string1 = sc.next();
+		System.out.print("Introduce another substring: ");
+		string2 = sc.next();
+		while(!string2.equalsIgnoreCase("end") || !string2.equalsIgnoreCase("fin")) {}
+		sc.close();
+	}
 }
 
 /*Introducir por teclado una frase palabra a palabra, y mostrar la frase completa separando
