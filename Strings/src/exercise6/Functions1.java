c248c5a13878b2b5f644b22bce86db793676c59c
aleleotta <alessandro.leotta@iesnervion.es>
2023-03-01 14:46:52 +0100

    .

---
+++ Strings/src/exercise6/Functions1.java
@@ -0,0 +1,19 @@
+package exercise6;
+import java.util.*;
+
+public class Functions1 {
+	
+	public static int wordCounterWithinString(String phrase, String word){
+		int count = 0;
+		int index = phrase.indexOf(word, 0);
+		while(index >= 0) { //Use lengths.
+			count++;
+			index = ;
+		}
+		return count;
+	}
+}
+
+/*Diseñar un programa que solicite al usuario una frase y una palabra.
+ *A continuación buscará cuántas veces aparece la palabra en la frase.
+ */
