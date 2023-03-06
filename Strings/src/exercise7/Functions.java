bfe338e125553a8c8f1cdaa9ea7f0a427e8ccf79
aleleotta <alessandro.leotta@iesnervion.es>
2023-03-06 09:45:13 +0100

    .

--- Strings/src/exercise7/Functions.java
+++ Strings/src/exercise7/Functions.java
@@ -4,10 +4,17 @@ public class Functions {
 	
 	public static boolean checkPalindrome(String phrase) {
 		boolean check = false;
-		for(int index = 0; index < phrase.length(); index++) {
-			if(indexOf(index) == lastIndexOf(index)) {
-				
+		int index1 = 0;
+		int index2 = phrase.length()-1;
+		while(!(index1 > phrase.length()) && index2 > 0) {
+			if(phrase.charAt(index1) != phrase.charAt(index2)) {
+				break;
 			}
+			index1++;
+			index2--;
+		}
+		if(index1 == phrase.length()-1 && index2 == 0) {
+			check = true;
 		}
 		return check;
 	}
